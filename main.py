73016a5deafbc496b03430856b5f81b687ef294f
femboyv <baumard.louane@gmail.com>
2025-07-13 18:29:21 +0200

    modified:   Weapons/Medium/Cannon/turret_01_mk1.png 	modified:   Weapons/Medium/Cannon/turret_04_mk1.png 	modified:   main.py

--- main.py
+++ main.py
@@ -40,6 +40,48 @@ BLUE = PALETTE[2]
 BLACK = PALETTE[3]
 
 
+## keyboard and mouse ##
+class keyboard_class:
+    def __init__(self):
+        self.pressed_keys = []
+        self.mouse_position = pygame.Vector2(pygame.mouse.get_pos())
+
+        self.LEFT_CLICK = "left click"
+        self.RIGHT_CLICK = "right click"
+        self.MIDDLE_CLICK = "middle click"
+        self.NEAR_CLICK = "near click"
+        self.FAR_CLICK = "far click"
+        self.click_map = {
+            0: "left click",
+            1: "right click",
+            2: "middle click",
+            3: "near click",  # the button on the side that's near the hand (fourth button)
+            4: "far click",  # the button on the side that's far to the hand (fifth button)
+        }
+
+    def key_press(self, key):
+        self.pressed_keys.append(pygame.key.name(key))
+
+    def key_release(self, key):
+        self.pressed_keys.remove(pygame.key.name(key))
+
+    def step(self):
+
+        self.mouse_position = pygame.Vector2(pygame.mouse.get_pos())
+
+        clicks_pressed = pygame.mouse.get_pressed(5)
+
+        for click_checked in range(5):
+            if clicks_pressed[click_checked]:
+                if self.click_map[click_checked] not in self.pressed_keys:
+                    self.pressed_keys.append(self.click_map[click_checked])
+            elif self.click_map[click_checked] in self.pressed_keys:
+                self.pressed_keys.remove(self.click_map[click_checked])
+
+
+keyboard = keyboard_class()
+
+
 ## manette ##
 class manette_class:
     def __init__(self, id, dead_zone_radius):
@@ -103,7 +145,7 @@ class manette_class:
                 self.buttons_pressed.append(self.key_map[button_number])
         # trigger are not button but axis
         # axis is at 0 at start, pressed it's a little bit less than 1 and released at -1
-        print(self.joysitck.get_axis(4))
+
         if self.joysitck.get_axis(4) > 0:  # left trigger
             self.buttons_pressed.append(self.key_map[16])
 
@@ -114,10 +156,63 @@ class manette_class:
 manette = manette_class(0, 0.3)
 
 
+## bullet ##
+
+all_bullets = []  # list of all instance of bullet (idk where to put it..)
+
+
+class bullet_class:
+    def __init__(
+        self,
+        x: float,
+        y: float,
+        sprite: pygame.Surface,
+        speed: float,
+        direction: pygame.Vector2,
+    ):
+        self.x = x
+        self.y = y
+        self.sprite = sprite
+        self.speed = speed
+        self.direction = direction
+        self.rect = sprite.get_rect()
+        self.exist = True
+        all_bullets.append(self)
+
+    def step(self):
+        if self.exist:
+            self.draw_self(self.direction)
+            self.x += self.direction.x * self.speed
+            self.y += self.direction.y * self.speed
+
+            if not screen.get_rect().collidepoint(
+                self.x, self.y
+            ):  # when out of borders (border of the screen)
+                self.destroy()
+
+    def draw_self(self, image_angle: pygame.Vector2):
+
+        rotated_sprite = pygame.transform.rotate(
+            self.sprite, -image_angle.as_polar()[1] - 90
+        )
+
+        rect_sprite_rotated = rotated_sprite.get_rect(center=(self.x, self.y))
+
+        screen.blit(
+            rotated_sprite,
+            rect_sprite_rotated,
+        )
+
+    def destroy(self):
+        self.exist = False
+        all_bullets.remove(self)
+
+
 ## player ##
 
 
 class player_class:
+
     def __init__(
         self,
         x: int,
@@ -126,15 +221,20 @@ class player_class:
         is_controlled_by_manette: bool,
         sprite: pygame.Surface,
         cannon_sprite: pygame.Surface,
+        bullet_sprite: pygame.Surface,
         speed_per_sec: int,
         cannon_speed_turn: float,
         sprite_speed_turn: float,
+        bullet_speed: float,
+        bullet_reload_time: float,
     ):
+
         self.x = x
         self.y = y
 
         self.sprite = sprite  # the spaceship
         self.cannon_sprite = cannon_sprite
+        self.bullet_sprite = bullet_sprite
 
         self.direction = pygame.Vector2.from_polar((1, direction))
         self.direction: pygame.Vector2
@@ -149,27 +249,37 @@ class player_class:
 
         self.cannon_speed_turn = cannon_speed_turn
         self.sprite_speed_turn = sprite_speed_turn
-
-        self.key_map = {
-            "A Button": "nothing",
-            "B Button": "nothing",
-            "X Button": "nothing",
-            "Y Button": "nothing",
-            "- Button": "nothing",
-            "Home Button": "nothing",
-            "+ Button": "nothing",
-            "L. Stick In": "nothing",
-            "R. Stick In": "nothing",
-            "Left Bumper": "nothing",
-            "Right Bumper": "nothing",
-            "D-pad Up": "nothing",
-            "D-pad Down": "nothing",
-            "D-pad Left": "nothing",
-            "D-pad Right": "nothing",
-            "Capture Button": "nothing",
-            "Left Trigger": "move",
-            "Right Trigger": "nothing",
-        }
+        self.bullet_speed = bullet_speed
+        self.bullet_reload_time = bullet_reload_time
+        self.tick_since_last_bullet = 0
+        if self.is_controlled_by_manette:
+            self.key_map = {
+                "A Button": "nothing",
+                "B Button": "nothing",
+                "X Button": "nothing",
+                "Y Button": "nothing",
+                "- Button": "nothing",
+                "Home Button": "nothing",
+                "+ Button": "nothing",
+                "L. Stick In": "nothing",
+                "R. Stick In": "nothing",
+                "Left Bumper": "nothing",
+                "Right Bumper": "nothing",
+                "D-pad Up": "nothing",
+                "D-pad Down": "nothing",
+                "D-pad Left": "nothing",
+                "D-pad Right": "nothing",
+                "Capture Button": "nothing",
+                "Left Trigger": "move",
+                "Right Trigger": "shoot",
+            }
+        else:
+            self.key_map = {
+                "d": "turn right",
+                "q": "turn left",
+                "z": "move",
+                "left click": "shoot",
+            }
 
     def draw_spaceship(self, image_angle: pygame.Vector2):
         rotated_sprite = pygame.transform.rotate(
@@ -194,21 +304,82 @@ class player_class:
             rect_cannon_sprite_rotated,
         )
 
-    def turning(
+    def turn_toward_vector(  ## coulb be patched
         self,
         vector_to_turn_toward: pygame.Vector2,
         vector_turning: pygame.Vector2,
         speed: float,
     ):
+
+        vector_output = vector_turning.copy()
         if vector_to_turn_toward != pygame.Vector2(0, 0):
-            vector_turning.move_towards_ip(vector_to_turn_toward, speed)
-            vector_turning.normalize_ip()
+            vector_output = vector_output.slerp(vector_to_turn_toward, speed)
+            vector_output.normalize_ip()
+
+        return vector_output  # to not return none when vector to turn toward is null
+
+    def turning(
+        self,
+        vector_to_turn_too: pygame.Vector2,
+        vector_turning: pygame.Vector2,
+        speed: float,
+    ):
+        speed_per_second = speed / fps  # in degrees
+
+        if (
+            vector_turning != vector_to_turn_too
+            and vector_to_turn_too != pygame.Vector2(0, 0)
+        ):
+
+            angle_between_vector = pygame.math.Vector2.angle_to(
+                vector_turning, vector_to_turn_too
+            )
+
+            if speed_per_second > abs(
+                angle_between_vector
+            ):  # anti vibration(must be before everything otherwise it don't work)
+                vector_turning = vector_to_turn_too
+
+            if angle_between_vector < 0:
+
+                if abs(angle_between_vector) < 180:
+                    output_vector = pygame.math.Vector2.rotate(
+                        vector_turning, -speed_per_second
+                    )
+
+                else:
+                    output_vector = pygame.math.Vector2.rotate(
+                        vector_turning, speed_per_second
+                    )
+
+            elif angle_between_vector > 0:
+
+                if abs(angle_between_vector) < 180:
+                    output_vector = pygame.math.Vector2.rotate(
+                        vector_turning, speed_per_second
+                    )
+                else:
+                    output_vector = pygame.math.Vector2.rotate(
+                        vector_turning, -speed_per_second
+                    )
+            return output_vector
+
+        else:
+            return vector_turning
+
+    def turn_right(self, vector_turning: pygame.Vector2, speed: float):
+
+        return vector_turning.rotate(speed)
+
+    def turn_left(self, vector_turning: pygame.Vector2, speed: float):
+
+        return vector_turning.rotate(-speed)
 
     def move(self, speed: pygame.Vector2):
         self.x += speed.x
         self.y += speed.y
 
-    def handle_button(self):
+    def handle_buttons_press(self):
 
         for button_pressed in manette.buttons_pressed:
             action_to_do = self.key_map[button_pressed]
@@ -216,27 +387,85 @@ class player_class:
             match action_to_do:
                 case "move":
                     self.move(self.sprite_angle)
+                case "shoot":
+                    self.shoot()
                 case "nothing":
                     "do nothing"
+
+    def handle_key_press(self):
+
+        for key_pressed in keyboard.pressed_keys:
+            if key_pressed in self.key_map:
+                action_to_do = self.key_map[key_pressed]
+
+                match action_to_do:
+                    case "move":
+                        self.move(self.sprite_angle)
+                    case "shoot":
+                        self.shoot()
+                    case "nothing":
+                        "do nothing"
+                    case "turn right":
+                        self.sprite_angle = self.turn_right(
+                            self.sprite_angle, self.sprite_speed_turn
+                        )
+                    case "turn left":
+                        self.sprite_angle = self.turn_left(
+                            self.sprite_angle, self.sprite_speed_turn
+                        )
+
+    def get_mouse_position_relative_to_player(self):
+        return pygame.Vector2(
+            keyboard.mouse_position[0] - self.x, keyboard.mouse_position[1] - self.y
+        )
+
+    def shoot(self):
+        if self.tick_since_last_bullet == self.bullet_reload_time:
+            self.tick_since_last_bullet = 0
+            return bullet_class(
+                self.x,
+                self.y,
+                self.bullet_sprite,
+                self.bullet_speed,
+                self.cannon_sprite_angle,
+            )
 
     def step(self):
 
         self.draw_spaceship(self.sprite_angle)
         self.draw_cannon(self.cannon_sprite_angle)
 
-        self.turning(
+        self.cannon_sprite_angle = self.turning(
             self.direction_cannon, self.cannon_sprite_angle, self.cannon_speed_turn
         )
-        self.turning(self.direction, self.sprite_angle, self.sprite_speed_turn)
+        self.sprite_angle = self.turning(
+            self.direction, self.sprite_angle, self.sprite_speed_turn
+        )
+
+        if self.tick_since_last_bullet < self.bullet_reload_time:
+            self.tick_since_last_bullet += 1
 
         if self.is_controlled_by_manette:
-            self.handle_button()
+
+            self.handle_buttons_press()
+
             self.direction = manette.movement_axis
 
             if manette.camera_axis == pygame.Vector2(0, 0):
                 self.direction_cannon = self.cannon_sprite_angle
             else:
                 self.direction_cannon = manette.camera_axis
+
+        else:  ## keyboard
+            self.handle_key_press()
+            self.cannon_sprite_angle = self.turning(
+                self.direction_cannon, self.cannon_sprite_angle, self.cannon_speed_turn
+            )
+            mouse_position_relative = self.get_mouse_position_relative_to_player()
+            if keyboard.mouse_position == pygame.Vector2(0, 0):
+                self.direction_cannon = self.cannon_sprite_angle
+            else:
+                self.direction_cannon = mouse_position_relative.normalize()
 
 
 player_manette = player_class(
@@ -246,20 +475,34 @@ player_manette = player_class(
     True,
     pygame.image.load("Ships\Medium\\body_02.png").convert_alpha(),
     pygame.image.load("Weapons\Medium\Cannon\\turret_04_mk1.png").convert_alpha(),
+    pygame.image.load("Weapons\Small\Cannon\\turret_01_bullet_01.png").convert_alpha(),
     50,
-    0.01,
-    0.005,
+    200,  # degrees per second
+    100,  # degrees per second
+    10,
+    10,
+)
+
+player_keyboard = player_class(
+    MIDDLE_SCREEN[0],
+    MIDDLE_SCREEN[1],
+    10,
+    False,
+    pygame.image.load("Ships\Medium\\body_03.png").convert_alpha(),
+    pygame.image.load("Weapons\Medium\Cannon\\turret_04_mk1.png").convert_alpha(),
+    pygame.image.load("Weapons\Small\Cannon\\turret_01_bullet_01.png").convert_alpha(),
+    50,
+    200,  # degrees per second
+    100,  # degrees per second
+    10,
+    10,
 )
 
 
 def debugging(activate: bool):
 
     if activate:
-        # print(
-        #     pygame.transform.rotate(
-        #         pygame.image.load("Weapons\Medium\Cannon\\turret_01_mk1.png"), 1
-        #     ).get_size()
-        # )
+        print(player_keyboard.sprite_angle)
         pygame.draw.line(
             screen,
             "red",
@@ -270,6 +513,7 @@ def debugging(activate: bool):
             ),
             5,
         )
+
         pygame.draw.line(
             screen,
             "green",
@@ -284,14 +528,25 @@ def debugging(activate: bool):
 
 while running:
 
+    for bullet in all_bullets:
+        bullet: bullet_class
+        bullet.step()
+
     debugging(1)
 
     manette.step()
+    keyboard.step()
     player_manette.step()
+    player_keyboard.step()
 
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             running = False
+        elif event.type == pygame.KEYDOWN:
+            keyboard.key_press(event.key)
+
+        elif event.type == pygame.KEYUP:
+            keyboard.key_release(event.key)
 
     pygame.display.flip()
     screen.fill("black")
